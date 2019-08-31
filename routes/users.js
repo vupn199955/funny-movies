e8ff4bf47fe36e58de72065076deab58ffa18cdb
Vu Pham <vupn199955@gmail.com>
2019-09-01 00:45:19 +0700

    Added new route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userController = require('../controllers').user;
+const JWT = require('../services/jsonwebtoken');
 const router = express.Router();
 
 router.post('/login', userController.login);
+
+router.get('/access-info', JWT.verifyToken, userController.getInfo);
 
 module.exports = router;
