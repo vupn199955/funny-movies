f9d0ad1b5d456865ce6b700eae9ec42836483ef4
Vu Pham <vupn199955@gmail.com>
2019-08-31 15:15:47 +0700

    Fixed: this.method is not a function

--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,30 @@
 const User = require('../models').Users;
 const JWT = require('../services/jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+function hashPassword(pass) {
+  return bcrypt.hashSync(pass, 10)
+};
+
+function comparePassword(pass, hash){
+  return bcrypt.compareSync(pass, hash);
+};
 
 module.exports = {
-  create(req, res) {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      return res.status(400).send("Missing field!");
-    }
-    const passHash = this.hashPassword(password);
+  login(req, res) {
     try {
+      const { username, password } = req.body;
+      if (!username || !password) {
+        return res.status(400).send("Missing field!");
+      }
+      const passHash = hashPassword(password);
+    
       User.findOrCreate(
         {where: { username }, defaults: { password: passHash }}
       )
       .spread((user, created) => {
         if (!created) {
-          const isCorrectPass = this.comparePassword(password, user.password);
+          const isCorrectPass = comparePassword(password, user.password);
           if (!isCorrectPass) {
             return res.status(401).send('Incorrent password!');
           }
@@ -25,11 +35,5 @@ module.exports = {
     } catch(err) {
       return res.status(500).send(err);
     };
-  },
-  hashPassword(pass) {
-    return bcrypt.hashSync(pass, 10)
-  },
-  comparePassword(pass, hash){
-    return bcrypt.compareSync(pass, hash);
   }
 }
